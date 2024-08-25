6cf1f8cb66bb7b093f0dcc1d0df569c60a9435cf
VASKO\eqinox <eqinox@abv.bg>
2024-08-25 20:00:03 +0300

    added middleware connection

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,6 +3,18 @@ module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": ["'self'", "data:", "blob:", "https://market-assets.strapi.io", "https://sweetsurprises-bucket.s3.eu-north-1.amazonaws.com"],
+          // Include other directives as needed
+        },
+      },
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
