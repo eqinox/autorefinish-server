64c31ddc4ffbbc18d5d3d191d6af425124607ff2
VASKO\eqinox <eqinox@abv.bg>
2024-08-27 09:31:20 +0300

    added aws sertificate just in case and some additional middleware in order to allow some operations

--- config/middlewares.js
+++ config/middlewares.js
@@ -10,7 +10,11 @@ module.exports = [
         useDefaults: true,
         directives: {
           "img-src": ["'self'", "data:", "blob:", "https://market-assets.strapi.io", "https://sweetsurprises-bucket.s3.eu-north-1.amazonaws.com"],
-          // Include other directives as needed
+          "script-src": ["'self'", "'unsafe-inline'", "blob:"], // Allow inline scripts and blob URIs
+          "worker-src": ["'self'", "blob:"], // Allow worker sources from blob URIs
+          "connect-src": ["'self'", "blob:", "https://sweetsurprises-bucket.s3.eu-north-1.amazonaws.com"], // Allow connections to specified sources
+          "default-src": ["'self'"], // Default fallback for other unspecified directives
+          "frame-src": ["'self'"], // Allow iframes from self (adjust if you use iframes from other sources)
         },
       },
     },
