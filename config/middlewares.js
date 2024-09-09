905f919401c3aac805d0d1a83f6b2c074a5ed03c
VASKO\eqinox <eqinox@abv.bg>
2024-09-09 08:12:02 +0300

    grant access for image preview in the dashboard

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,10 +9,10 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "img-src": ["'self'", "data:", "blob:", "https://market-assets.strapi.io", "https://sweetsurprises-bucket.s3.eu-north-1.amazonaws.com"],
+          "img-src": ["'self'", "data:", "blob:", "https://market-assets.strapi.io", "https://autorefinish.s3.eu-north-1.amazonaws.com"],
           "script-src": ["'self'", "'unsafe-inline'", "blob:"], // Allow inline scripts and blob URIs
           "worker-src": ["'self'", "blob:"], // Allow worker sources from blob URIs
-          "connect-src": ["'self'", "blob:", "https://sweetsurprises-bucket.s3.eu-north-1.amazonaws.com"], // Allow connections to specified sources
+          "connect-src": ["'self'", "blob:", "https://autorefinish.s3.eu-north-1.amazonaws.com"], // Allow connections to specified sources
           "default-src": ["'self'"], // Default fallback for other unspecified directives
           "frame-src": ["'self'"], // Allow iframes from self (adjust if you use iframes from other sources)
         },
